a495f8b34ecbd0b15c5edf297b83e4a98376bdd5
unknown <heyreza8@gmail.com>
2025-06-08 14:04:39 +0330

    keep coding Todo List Component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,56 @@
+import {type ChangeEvent, type FormEvent, useState} from "react";
+
+interface Todo{
+    id: number;
+    name: string;
+    completed: boolean;
+}
+
 const TodoList = () => {
+    const [todos,setTodos] = useState<Todo[]>([]);
+    const [newTodo,setNewTodo] = useState<string>("")
+
+    const handleAddTodo = (event : FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if(newTodo != null){
+            setTodos([
+                ...todos,
+                {
+                    id : Math.floor(Math.random() * 100),
+                    name : newTodo,
+                    completed : false
+                }
+            ])
+
+            setNewTodo("")
+        }
+    }
+
+    const completeTodo = (id : number) => {
+        const todoIndex = todos.findIndex(todo => todo.id === id)
+        const updatedTodo =
+            todos[todoIndex] = {
+            ...todos[todoIndex],
+            completed: !todos[todoIndex].completed
+        };
+        let updatedTodos = [...todos];
+        updatedTodos[todoIndex] = updatedTodo
+
+        setTodos(updatedTodos)
+
+    }
+
     return (
         <div>
             <h1>
                 Todo Lists
             </h1>
-            <form className={"space"}>
+            <form className={"space"} onSubmit={handleAddTodo}>
                 <label>Todo Name</label>
                 <br/>
-                <input type={"text"} />
+                <input type={"text"} onChange={(event: ChangeEvent<HTMLInputElement>) => setNewTodo((event.target.value))} value={newTodo} />
+                <button type={"submit"}>add</button>
             </form>
             <table className={"App"}>
                 <thead>
@@ -24,24 +67,19 @@ const TodoList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>3</td>
-                        <td>name</td>
-                        <td>
-                            <button className={"red"}>
-                                not Completed
-                            </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>name</td>
-                        <td>
-                            <button className={"green"}>
-                                Completed
-                            </button>
-                        </td>
-                    </tr>
+                {
+                    todos.map(todo => (
+                        <tr key={todo.id}>
+                            <td>{todo.id}</td>
+                            <td>{todo.name}</td>
+                            <td>
+                                <button onClick={() => completeTodo(todo.id)} className={todo.completed ? "green" : "red"}>
+                                    {todo.completed ? "Yes" : "No"}
+                                </button>
+                            </td>
+                        </tr>
+                    ))
+                }
                 </tbody>
             </table>
         </div>
