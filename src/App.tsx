9407b448d0a31f5afc8327a80fc897b81d8104ab
unknown <heyreza8@gmail.com>
2025-06-09 17:09:34 +0330

    install react router dom and setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import Header from "./components/Header.tsx";
 import Todos from "./components/Todos.tsx";
 import AddTodo from "./components/AddTodo.tsx";
 import EditTodo from "./components/EditTodo.tsx";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
         <div className={"h-screen w-screen bg-gray-900"}>
             <div className={"container mx-auto pt-4"}>
                 <Header />
+                <Routes>
+                    <Route path={"/"} element={<Navigate to={"/todos"} />} />
+                    <Route path={"/todos"} element={<Todos />} />
+                    <Route path={"/todos/add"} element={<AddTodo />} />
+                    <Route path={"/todos/:id/edit"} element={<EditTodo />} />
+                </Routes>
             </div>
         </div>
     )
