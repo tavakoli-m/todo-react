88fae88ef0f30678a39d0d80e3d869fca57f91ec
unknown <heyreza8@gmail.com>
2025-06-09 17:18:12 +0330

    replace todos component header with search input

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -10,9 +10,10 @@ const Todos = () => {
                     کار جدید
                 </Link>
 
-                <h2 className={"text-purple-600 text-base"}>
-                    لیست کار ها
-                </h2>
+
+                <input type="text" id="search"
+                       className="bg-gray-50 border max-w-46 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       placeholder="جستجو"/>
             </div>
 
             <div className={"flex justify-between mt-20 text-sm text-white"}>
